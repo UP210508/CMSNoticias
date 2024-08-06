a52c13ea0c5dc3e5ee86dcee87352eb96d755729
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-08-06 00:09:15 -0600

    Login Design done

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './modules/auth/login/Login';
+import { Register } from './modules/auth/register/Register';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='login' element={<Login />} />
+        <Route path='registrar' element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/modules/auth/register/Register.jsx
+++ frontend/src/modules/auth/register/Register.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import { LayoutAuth } from '../layout/LayoutAuth';
 
-export const Login = () => {
-
+export const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <LayoutAuth title='Bievenido'>
+    <LayoutAuth title="Comienza tu viaje">
       <form className="form form--login">
+        <div className="form__field">
+          <label className="form__label">Nombre</label>
+          <input
+            placeholder="Ingresa tu nombre"
+            name="email"
+            className="form__input"
+            type="email"
+          />
+        </div>
         <div className="form__field">
           <label className="form__label">Email</label>
           <input
-            placeholder="correo@correo.com"
+            placeholder="Ingresa un correo válido"
             name="email"
             className="form__input"
             type="email"
@@ -21,7 +29,24 @@ export const Login = () => {
           <label className="form__label">Contraseña</label>
           <div className="form__password">
             <input
-              placeholder="Ingrese su contraseña"
+              placeholder="Ingrese una contraseña"
+              name="password"
+              className="form__input"
+              type={showPassword ? 'text' : 'password'}
+            />
+            <i
+              onClick={() => setShowPassword(!showPassword)}
+              className={`bx bx-${
+                showPassword ? 'hide' : 'show'
+              } icon icon--blue login__icon`}
+            ></i>
+          </div>
+        </div>
+        <div className="form__field">
+          <label className="form__label">Confirma tu Contraseña</label>
+          <div className="form__password">
+            <input
+              placeholder="Ingrese una contraseña"
               name="password"
               className="form__input"
               type={showPassword ? 'text' : 'password'}
