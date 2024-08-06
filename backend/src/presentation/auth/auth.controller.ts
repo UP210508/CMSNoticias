fb1112b92f2b286afa3d395740a8a9c4642e25c3
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-08-06 17:04:27 -0600

    Post user service started

--- backend/src/presentation/auth/auth.controller.ts
+++ backend/src/presentation/auth/auth.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
 import { AuthServices } from "./auth.services";
 import { CreateUserDto } from "../../domain/dto/users/create-user.dto";
+import { CustomError } from "../../domain/errors/custom.error";
 
 export class AuthController {
 
   constructor(
     private readonly authService: AuthServices
   ){}
+
+  private handleErrorResponse = ( error: unknown, res: Response ) => {
+
+    if ( error instanceof CustomError ) {
+      return res.status( error.statusCode ).json({ error: error.message });
+    }
+
+    console.log(`${error}`);
+    return res.status(500).json({ error: 'Internal server error'});
+
+  }
 
   public loginUser = ( req: Request, res: Response ) => {
     res.json({ msg: 'Hello'})
@@ -15,6 +27,11 @@ export class AuthController {
   public registerUser = ( req: Request, res: Response ) => {
     const [ errorMessage, createUserDto ] = CreateUserDto.create( req.body );
 
+    if ( errorMessage ) {
+      return res.status(400).json({ error: errorMessage });
+    }
+
+    // this.authService.postUser( createUserDto! )
 
   }
 
--- backend/src/presentation/auth/auth.services.ts
+++ backend/src/presentation/auth/auth.services.ts
@@ -1,5 +1,19 @@
+import { UserModel } from "../../data";
+import { CreateUserDto } from "../../domain/dto/users/create-user.dto";
+import { CustomError } from "../../domain/errors/custom.error";
+
 export class AuthServices {
 
-  
+  public async postUser( createUserDto: CreateUserDto ) {
+
+    const emailExistence = await UserModel.findOne({ email: createUserDto.email });
+
+    if ( emailExistence ) {
+      throw CustomError.badRequest('El usuario ya existe');
+    }
+
+
+
+  } 
 
 }
