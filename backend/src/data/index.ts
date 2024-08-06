50ca5961277d0d748ef7e9b73e0c8de2bbac2029
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-08-06 16:32:41 -0600

    Models done

---
+++ backend/src/data/index.ts
@@ -0,0 +1,3 @@
+export * from './mongo-db/models/user.model';
+export * from './mongo-db/models/category.model';
+export * from './mongo-db/models/notice.model';
---
+++ backend/src/data/mongo-db/models/user.model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+
+  firstName: {
+    type: String,
+    required: [ true, 'Nombre obligatorio'],
+    maxlength: 60,
+  },
+
+  lastName: {
+    type: String,
+    maxlength: 60,
+  },
+
+  email: {
+    type: String,
+    required: [ true, 'Email obligatorio'],
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: [ true, 'Password obligatorio'],
+    minlength: 8,
+  },
+
+  phone: {
+    type: String,
+    maxlength: 10,
+    unique: true,
+  },
+
+  createdAt: {
+    type: String,
+    required: [ true, 'Creado a obligatorio'],
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+
+  img: {
+    type: String,
+  }
+
+});
+
+UserSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function( _doc, ret, _options ) {
+    delete ret._id;
+    delete ret.password;
+  }
+})
+
+export const UserModel = mongoose.model('User', UserSchema );
