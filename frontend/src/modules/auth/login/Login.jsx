741b808cc04bf9c122059722b437e87cae7a5c03
Danna Janeth Sanchez Carreon <up210508@alumnos.upa.edu.mx>
2024-08-06 21:06:31 -0600

    Login Form Validations done

--- frontend/src/modules/auth/login/Login.jsx
+++ frontend/src/modules/auth/login/Login.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react';
 import { LayoutAuth } from '../layout/LayoutAuth';
+import { useForm } from '../../hooks/useForm';
+
+const formData = {
+  email: "",
+  password: "",
+}
+
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+const formValidations = {
+  email: [ value => emailRegex.test( value ), "El email es obligatorio"],
+  password: [ value => value.length > 1, "La contraseña es obligatoria"]
+}
 
 export const Login = () => {
-
   const [showPassword, setShowPassword] = useState(false);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const {
+    email,
+    emailValid,
+    password,
+    passwordValid,
+    onInputChange,
+    onResetForm,
+    isFormValid,
+    formState
+  } = useForm(formData, formValidations);
+
+  const onLoginUser = (e) => {
+    e.preventDefault();
+    setIsFormSubmitted( true );
+
+    if ( !isFormValid ) return;
+
+
+    setIsFormSubmitted( false );
+  }
 
   return (
     <LayoutAuth title='Bievenido'>
-      <form className="form form--login">
+      <form onSubmit={ onLoginUser } className="form form--login">
         <div className="form__field">
           <label className="form__label">Email</label>
           <input
@@ -16,6 +49,13 @@ export const Login = () => {
             className="form__input"
             type="email"
           />
+          <span
+              className={`form__span ${
+                !isFormValid && isFormSubmitted ? 'text-wrong' : null
+              }`}
+            >
+            {emailValid}
+          </span>
         </div>
         <div className="form__field">
           <label className="form__label">Contraseña</label>
@@ -33,6 +73,13 @@ export const Login = () => {
               } icon icon--blue login__icon`}
             ></i>
           </div>
+          <span
+              className={`form__span ${
+                !isFormValid && isFormSubmitted ? 'text-wrong' : null
+              }`}
+            >
+            {passwordValid}
+          </span>
         </div>
         <div className="form__button">
           <button className="btn btn--blue">Ingresar</button>
